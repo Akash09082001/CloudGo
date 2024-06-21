c1d12aa6d1aafd57281865c3ec5ab3e51ade817e
Akash09082001 <aakashl@letsupgrade.in>
2024-06-21 15:40:12 +0530

    added changes

---
+++ src/app/loading.jsx
@@ -0,0 +1,13 @@
+import Logo from "@/components/atoms/Logo"
+
+const loading = () => {
+    return (
+        <div className="flex w-full flex-1 flex-grow items-center justify-center">
+
+            <Logo />
+
+        </div>
+    )
+}
+
+export default loading
